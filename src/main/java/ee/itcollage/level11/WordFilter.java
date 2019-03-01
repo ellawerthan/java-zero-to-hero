8beba123ea778dc56978244a9badcd00c508faa1
Ella <ellawerthan@gmail.com>
2019-03-01 15:49:44 +0200

    Exercise progress (Level 11)

--- src/main/java/ee/itcollage/level11/WordFilter.java
+++ src/main/java/ee/itcollage/level11/WordFilter.java
@@ -8,30 +8,60 @@ public class WordFilter {
     //todo fix tests
 
     public List<Word> getNouns(List<Word> words) {
-        return words;
+        List<Word> nouns = new ArrayList<>();
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).getWordType() == Word.WordType.NOUN) {
+                nouns.add(words.get(i));
+            }
+        }
+        return nouns;
     }
 
     public Word getFirstVerb(List<Word> words) {
-        return words.get(0);
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).getWordType() == Word.WordType.VERB) {
+                return words.get(i);
+            }
+        }
+        return null;
     }
 
     public Word getFirstOfType(List<Word> words, Word.WordType type) {
-        return words.get(0);
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).getWordType() == type) {
+                return words.get(i);
+            }
+        }
+        return null;
     }
 
     public List<Word> getAllNotNouns(List<Word> words) {
-        return words;
+        List<Word> notNouns = new ArrayList<>();
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).getWordType() != Word.WordType.NOUN) {
+                notNouns.add(words.get(i));
+            }
+        }
+        return notNouns;
     }
 
     public List<String> getNounStrings(List<Word> words){
-        return new ArrayList<>();
+        List<Word> nouns = getNouns(words);
+        List<String> nounStrings = new ArrayList<>();
+        for (int i = 0; i < nouns.size(); i++) {
+            nounStrings.add(nouns.get(i).getWord());
+        }
+        return nounStrings;
     }
 
     public String getFirstVerbString(List<Word> words) {
-        return "";
+        Word firstVerb = getFirstVerb(words);
+        String firstVerbString = firstVerb.getWord();
+        return firstVerbString;
     }
 
     public String getFirstStringOfType(List<Word> words, Word.WordType type) {
-        return "";
+        Word first = getFirstOfType(words, type);
+        return first.getWord();
     }
 }
