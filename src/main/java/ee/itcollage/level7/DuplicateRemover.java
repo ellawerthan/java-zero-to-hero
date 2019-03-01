de9c52b50c40f49fea11619d501768f67118870e
Ella <ellawerthan@gmail.com>
2019-03-01 14:25:06 +0200

    Exercise progress (Level 7)

--- src/main/java/ee/itcollage/level7/DuplicateRemover.java
+++ src/main/java/ee/itcollage/level7/DuplicateRemover.java
@@ -1,11 +1,24 @@
 package ee.itcollage.level7;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DuplicateRemover {
 
     //todo fix tests
-    public static List<Integer> removeDuplicates(List<Integer> numbers){
-        return numbers;
+    public static List<Integer> removeDuplicates(List<Integer> numbers) {
+        List<Integer> noDuplicates = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            boolean duplicate = false;
+            for (int j = 0; j < noDuplicates.size(); j++) {
+                if (numbers.get(i) == noDuplicates.get(j)) {
+                    duplicate = true;
+                }
+            }
+            if (!duplicate) {
+                noDuplicates.add(numbers.get(i));
+            }
+        }
+        return noDuplicates;
     }
 }
