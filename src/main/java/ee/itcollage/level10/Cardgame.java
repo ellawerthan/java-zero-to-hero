633532b5411bd80ddc53434bdc6fae241c5d9d42
Ella <ellawerthan@gmail.com>
2019-03-01 15:07:14 +0200

    Exercise progress (Level 10)

--- src/main/java/ee/itcollage/level10/Cardgame.java
+++ src/main/java/ee/itcollage/level10/Cardgame.java
@@ -8,6 +8,12 @@ public class Cardgame {
 
     public static List<Card> buildDeck() {
         List<Card> cards = new ArrayList<>();
+        for (Rank rank : Rank.values()) {
+           for (Suit suit : Suit.values()) {
+               Card myCard = new Card(rank, suit);
+               cards.add(myCard);
+           }
+       }
         return cards;
     }
 
@@ -19,6 +25,13 @@ public class Cardgame {
     public static void main(String[] args) {
         //todo 1 fix tests
         //todo 2 simulate playing poker (buildDeck, shuffle and get 5 cards)
-
+        Cardgame myGame = new Cardgame();
+        List<Card> myDeck = myGame.buildDeck();
+        myDeck = myGame.shuffle(myDeck);
+        List<Card> myHand = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            myHand.add(myDeck.get(i));
+        }
+        System.out.println(myHand);
     }
 }
