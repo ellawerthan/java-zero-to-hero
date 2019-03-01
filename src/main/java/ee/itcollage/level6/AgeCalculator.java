8b21fec2fd37d7d982aa3f3c87e43347c2073f1b
Ella <ellawerthan@gmail.com>
2019-03-01 14:17:42 +0200

    Exercise progress (Level 6)

--- src/main/java/ee/itcollage/level6/AgeCalculator.java
+++ src/main/java/ee/itcollage/level6/AgeCalculator.java
@@ -8,11 +8,31 @@ public class AgeCalculator {
         System.out.println("How old are you?");
 
         Scanner sc = new Scanner(System.in);
-        int insertedInt = sc.nextInt(); //or sc.nextLine() for String
-        System.out.println("You are: " + insertedInt + " years old");
+        int age = sc.nextInt(); //or sc.nextLine() for String
+        System.out.println("You are: " + age + " years old");
 
         // todo calculate and print which year person was born in
+        int birthYear = 2018 - age;
+        System.out.println("You were born in "  + birthYear + ".");
+
         // todo if person is 18+ ask them whether they have coded java
-        // todo if yes, do sth, if no, do sth else
+        if (age >= 18) {
+            sc = new Scanner(System.in);
+            System.out.println("Have you coded java? (y/n)");
+            String answer = sc.nextLine();
+            boolean java;
+            if (answer.equals("y")) {
+                java = true;
+            } else {
+                java = false;
+            }
+
+            // todo if yes, do sth, if no, do sth else
+            if(java) {
+                System.out.println("Nice!");
+            } else {
+                System.out.println("Boo!");
+            }
+        }
     }
 }
