21fa05cbe08d6962771acae8eb4cd97282e39fd4
Ella <ellawerthan@gmail.com>
2019-03-01 13:57:53 +0200

    Exercise progress (Level 0, Level 2)

--- src/main/java/ee/itcollage/level2/Multiplication.java
+++ src/main/java/ee/itcollage/level2/Multiplication.java
@@ -2,8 +2,14 @@ package ee.itcollage.level2;
 
 public class Multiplication {
 
+    public static int multiply(int x1, int x2) {
+        return x1 * x2;
+    }
+
     public static void main(String[] args) {
         //todo create function in this class that multiplies 2 inputs
         // call the function in the main method, for example with 5x4... should be 20
+        int result = multiply(5, 4);
+        System.out.printf("5 times 4 is %d", result);
     }
 }
